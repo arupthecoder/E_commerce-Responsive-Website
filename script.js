7f17bef681f90cb5302e89119cc12881d10aa57f
arupthecoder <119742142+arupthecoder@users.noreply.github.com>
2022-12-24 13:37:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// alert('hello')
+const bar = document.getElementById('bar');
+const closed = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (closed) {
+    closed.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
